f8dfba6ed57d3312fdc02404980ec74c85ccc220
smth-hyj <smth.hyj@gmail.com>
2024-04-14 13:19:32 +0000

    update(1)

--- rspgame.py
+++ rspgame.py
@@ -14,4 +14,5 @@ def is_win(player, opponent):
     if (player == 'r' and opponent == 's') or (player == 'p' and opponent == 'r') or (player == 's' and opponent == 'p'):
         return true
 
+print("컴퓨터의 선택은 : ", computer)
 print(play())
